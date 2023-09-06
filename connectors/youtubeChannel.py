5c77735dd41edc32704b6ec893a4567e9e4785d1
Cole Challand <ctchalland@gmail.com>
2023-09-06 03:03:52 -0400

    added save json to file func

--- connectors/youtubeChannel.py
+++ connectors/youtubeChannel.py
@@ -1,3 +1,4 @@
+import json
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -8,18 +9,24 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class Channel:
     def __init__(self, url) -> None:
+        self._channel_name = ""
         self._channel_url = url
+        self._filename = ""
         self._data = []
+        self._data_len = 0
         self._items = []
         self._item_count = 180
         self.SCROLL_PAUSE_TIME = 1
         
-        self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Firefox()
+        self.driver = webdriver.Firefox('/Users/ColeChalland/Documents/Selenium/FireFox')
         self.driver.get(url)
         self._start_up()
 
 
+
     def _start_up(self) -> None:
+        self._channel_name = self.driver.find_element(By.CSS_SELECTOR,'yt-formatted-string#text').text
         time.sleep(3)
         last_height = self.driver.execute_script("return document.documentElement.scrollHeight")
         
@@ -37,15 +44,27 @@ class Channel:
                 vurl = e.find_element(By.CSS_SELECTOR,'a#video-title-link').get_attribute('href')
                 views= e.find_element(By.XPATH,'.//*[@id="metadata"]//span[@class="inline-metadata-item style-scope ytd-video-meta-block"][1]').text
                 date_time = e.find_element(By.XPATH,'.//*[@id="metadata"]//span[@class="inline-metadata-item style-scope ytd-video-meta-block"][2]').text
-                self._data.append({'video_url':vurl, 'title':title, 'date_time':date_time, 'views':views})
+                self._data.append({'video_url': vurl, 'title': title, 'date_time': date_time, 'views': views})
         except:
             pass
-        print(self._data)
-        print(len(self._data))
+        
+        self._data_len = len(self._data)
+
+
+
+    def save_json(self) -> None:
+        self._filename = self._channel_name + ".json"
+        # Serializing json
+        json_object = json.dumps(self._data, indent=4)
+        # Writing to sample.json
+        with open(self._filename, "w") as outfile:
+            outfile.write(json_object)
 
 
     def stop_driver(self) -> None:
         self.driver.quit()
+
+
 
 
 if __name__ == '__main__':  # Execute the following code only when executing main.py (not when importing it)
